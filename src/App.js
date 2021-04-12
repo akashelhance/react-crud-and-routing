a1fd76d7841dd796de9abf9e1e608bc8fe2936de
akash elhance <akashelhance@gmail.com>
2021-04-12 17:37:33 +0530

    Components and layouts added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import { Navbar } from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import NotFound from './components/pages/NotFound';
 
 function App() {
   return (
     <div className="App">
-     <h1 className="text-center"> Hello World</h1>
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Contact/>
+      <NotFound/>
     </div>
   );
 }
